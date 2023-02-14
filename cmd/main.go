61d3e1f41f40b6bd446e1b78f409813f8d057a70
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-02-15 00:13:15 +0100

    [UPD] Entry

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"github.com/plaenkler/ddns/pkg/database"
+	"github.com/plaenkler/ddns/pkg/router"
+)
+
+func init() {
+	database.GetManager().Start()
+}
+
+func main() {
+	router.GetManager().Start()
+}
