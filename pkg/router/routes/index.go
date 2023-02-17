9b189595ce840f4be734784b1fc3cdce7dea0495
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-02-17 03:37:14 +0100

    [UPD] Transmit entries

--- pkg/router/routes/index.go
+++ pkg/router/routes/index.go
@@ -6,12 +6,19 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/plaenkler/ddns/pkg/database"
+	"github.com/plaenkler/ddns/pkg/model"
 )
 
 var (
 	//go:embed static
 	static embed.FS
 )
+
+type structIndex struct {
+	Jobs []model.Updater
+}
 
 func ProvideIndex(writer http.ResponseWriter, request *http.Request) {
 	// Default to 404
@@ -23,19 +30,27 @@ func ProvideIndex(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusForbidden)
 		_, err := writer.Write([]byte("[provide-index-2] 405 - Method not allowed"))
 		if err != nil {
-			log.Printf("[provide-index-3] could not write http reply - error: %s", err)
+			log.Printf("[provide-index-2] could not write http reply - error: %s", err)
 		}
 		return
 	}
 	template, err := template.New("index").ParseFS(static,
-		"static/html/index.html",
+		"static/html/pages/index.html",
+		"static/html/partials/include.html",
+		"static/html/partials/navbar.html",
 	)
 	if err != nil {
-		fmt.Fprintf(writer, "[provide-index-4] could not provide template - error: %s", err)
+		fmt.Fprintf(writer, "[provide-index-3] could not provide template - error: %s", err)
+		return
+	}
+	structIndex := structIndex{}
+	err = database.GetManager().DB.Find(&structIndex.Jobs).Error
+	if err != nil {
+		fmt.Fprintf(writer, "[provide-index-4] could not find jobs - error: %s", err)
 		return
 	}
 	writer.Header().Add("Content-Type", "text/html")
-	err = template.Execute(writer, nil)
+	err = template.Execute(writer, structIndex)
 	if err != nil {
 		fmt.Fprintf(writer, "[provide-index-5] could not execute parsed template - error: %v", err)
 	}
