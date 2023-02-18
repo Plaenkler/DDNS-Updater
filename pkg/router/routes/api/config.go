41537e118989293dc94608ee40d16237385ca807
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-02-18 18:31:26 +0100

    [FIX] Switch not required

--- pkg/router/routes/api/config.go
+++ pkg/router/routes/api/config.go
@@ -10,37 +10,42 @@ import (
 )
 
 func UpdateConfig(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodPost:
-		err := r.ParseForm()
-		if err != nil {
-			log.Printf("[api-handleConfig-1] could not parse form err: %s", err.Error())
-		}
-		// 1 - 65535
-		if len(r.FormValue("port")) < 1 || len(r.FormValue("port")) > 5 {
-			log.Printf("[api-handleConfig-2] port is not valid")
-		}
-		if len(r.FormValue("interval")) < 1 {
-			log.Printf("[api-handleConfig-3] interval is not valid")
-		}
-		port, err := strconv.ParseUint(r.FormValue("port"), 10, 16)
-		if err != nil {
-			log.Printf("[api-handleConfig-4] port is not valid - error: %s", err)
-		}
-		interval, err := strconv.ParseUint(r.FormValue("interval"), 10, 16)
-		if err != nil {
-			log.Printf("[api-handleConfig-5] interval is not valid - error: %s", err)
-		}
-		config.UpdateConfig(&model.Config{
-			Port:     port,
-			Interval: interval,
-		})
-		http.Redirect(w, r, r.Header.Get("Referer"), http.StatusTemporaryRedirect)
-	default:
+	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		_, err := w.Write([]byte(http.StatusText(http.StatusMethodNotAllowed)))
-		if err != nil {
-			log.Printf("[api-handleConfig-6] could not write http reply - error: %s", err)
-		}
+		return
 	}
+	err := r.ParseForm()
+	if err != nil {
+		log.Printf("[api-handleConfig-1] could not parse form err: %s", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// 1 - 65535
+	if len(r.FormValue("port")) < 1 || len(r.FormValue("port")) > 5 {
+		log.Printf("[api-handleConfig-2] port is not valid")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if len(r.FormValue("interval")) < 1 {
+		log.Printf("[api-handleConfig-3] interval is not valid")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	port, err := strconv.ParseUint(r.FormValue("port"), 10, 16)
+	if err != nil {
+		log.Printf("[api-handleConfig-4] port is not valid - error: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	interval, err := strconv.ParseUint(r.FormValue("interval"), 10, 16)
+	if err != nil {
+		log.Printf("[api-handleConfig-5] interval is not valid - error: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	config.UpdateConfig(&model.Config{
+		Port:     port,
+		Interval: interval,
+	})
+	http.Redirect(w, r, r.Header.Get("Referer"), http.StatusTemporaryRedirect)
 }
