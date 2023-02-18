8c489ec868a3d166e3c6a364770482465bc114a7
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-02-18 03:03:02 +0100

    [FIX] Reconstructed models

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,16 +1,25 @@
 package model
 
-type User struct {
-	ID       int64
-	Username string
-	Password string
+import (
+	"gorm.io/gorm"
+)
+
+type SyncJob struct {
+	gorm.Model
+	Provider    string    `gorm:"not null"`
+	Domain      string    `gorm:"not null;unique"`
+	User        string    `gorm:"not null"`
+	Password    string    `gorm:"not null"`
+	IPAddress   IPAddress `gorm:"foreignKey:IPAddressID"`
+	IPAddressID *uint64
 }
 
-type Updater struct {
-	ID         int64
-	Provider   string
-	Domain     string
-	User       string
-	Password   string
-	LastIPAddr string
+type IPAddress struct {
+	gorm.Model
+	Address string `gorm:"not null;unique"`
+}
+
+type Config struct {
+	Port     uint64 `yaml:"Port"`
+	Interval uint64 `yaml:"Interval"`
 }
