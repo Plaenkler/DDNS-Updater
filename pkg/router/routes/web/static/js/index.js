b8b6d3eabced20972725f15706437b1481b5d44d
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-02-18 19:54:07 +0100

    [FIX] Standard JS format

--- pkg/router/routes/web/static/js/index.js
+++ pkg/router/routes/web/static/js/index.js
@@ -1,11 +1,13 @@
-document.querySelectorAll("table tbody tr").forEach(row => {
-  row.addEventListener("click", event => {
-    const [id, provider, domain, user] = event.target.parentNode.children;
-    document.querySelector("#edit-id").value = id.textContent;
-    document.querySelector("#edit-provider").value = provider.textContent;
-    document.querySelector("#edit-domain").value = domain.textContent;
-    document.querySelector("#edit-user").value = user.textContent;
-    document.querySelector("#delete-button").href = `/api/job/delete?ID=${id.textContent}`;
-    new bootstrap.Modal(document.querySelector("#edit-modal")).show();
-  });
-});
+document.querySelectorAll('table tbody tr').forEach((row) => {
+    row.addEventListener('click', (event) => {
+        const [id, provider, domain, user] = event.target.parentNode.children
+        document.querySelector('#edit-id').value = id.textContent
+        document.querySelector('#edit-provider').value = provider.textContent
+        document.querySelector('#edit-domain').value = domain.textContent
+        document.querySelector('#edit-user').value = user.textContent
+        document.querySelector(
+            '#delete-button'
+        ).href = `/api/job/delete?ID=${id.textContent}`
+        new bootstrap.Modal(document.querySelector('#edit-modal')).show()
+    })
+})
