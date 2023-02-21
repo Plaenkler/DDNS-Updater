757d1c8ae92ba077d5cb87520fef5861a573a6bf
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-02-21 14:31:42 +0100

    [UPD] Start logs

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/plaenkler/ddns/pkg/database"
 	"github.com/plaenkler/ddns/pkg/ddns"
 	"github.com/plaenkler/ddns/pkg/router"
@@ -8,9 +10,11 @@ import (
 
 func init() {
 	database.GetManager().Start()
+	log.Printf("[main-init-1] database started")
 }
 
 func main() {
 	go ddns.Run()
+	log.Printf("[main-main-1] ddns service started")
 	router.GetManager().Start()
 }
