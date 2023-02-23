a34460ebdc0ac855115325ac95cb55ed1e9b3145
Simon Lukas <60503970+Plaenkler@users.noreply.github.com>
2023-02-23 13:39:10 +0100

    [UPD] Obtain provider -> pkg providers

--- pkg/ddns/update.go
+++ pkg/ddns/update.go
@@ -1,252 +1,60 @@
 package ddns
 
-import (
-	"bytes"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"strings"
-	"time"
+import "github.com/plaenkler/ddns/pkg/ddns/providers"
 
-	"github.com/plaenkler/ddns/pkg/model"
-)
+type Updater func(request interface{}) error
 
-type Update func(job model.SyncJob, ipAddr string) error
-
-var updaters = map[string]Update{
-	"Strato":       updateStrato,
-	"DDNSS":        updateDDNSS,
-	"Dynu":         updateDynu,
-	"Aliyun":       updateAliyun,
-	"AllInkl":      updateAllInkl,
-	"Cloudflare":   updateCloudflare,
-	"DD24":         updateDD24,
-	"DigitalOcean": updateDigitalOcean,
-	"DonDominio":   updateDonDominio,
-	"DNSOMatic":    updateDNSOMatic,
-	"DNSPod":       updateDNSPod,
-	"Dreamhost":    updateDreamhost,
-	"DuckDNS":      updateDuckDNS,
-	"DynDNS":       updateDynDNS,
-	"FreeDNS":      updateFreeDNS,
-	"Gandi":        updateGandi,
-	"GCP":          updateGCP,
-	"GoDaddy":      updateGoDaddy,
-	"Google":       updateGoogle,
-	"He":           updateHe,
-	"Infomaniak":   updateInfomaniak,
-	"INWX":         updateINWX,
-	"Linode":       updateLinode,
-	"LuaDNS":       updateLuaDNS,
-	"Namecheap":    updateNamecheap,
-	"NoIP":         updateNoIP,
-	"Njalla":       updateNjalla,
-	"OpenDNS":      updateOpenDNS,
-	"OVH":          updateOVH,
-	"Porkbun":      updatePorkbun,
-	"Selfhost":     updateSelfhost,
-	"Servercow":    updateServercow,
-	"Spdyn":        updateSpdyn,
-	"Variomedia":   updateVariomedia,
+type Provider struct {
+	Updater Updater
+	Request func() interface{}
 }
 
-func IsUpdaterSupported(updater string) bool {
-	_, ok := updaters[updater]
+var updaters = map[string]Provider{
+	"Strato":       {Updater: providers.UpdateStrato, Request: func() interface{} { return &providers.UpdateStratoRequest{} }},
+	"DDNSS":        {Updater: providers.UpdateDDNSS, Request: func() interface{} { return &providers.UpdateDDNSSRequest{} }},
+	"Dynu":         {Updater: providers.UpdateDynu, Request: func() interface{} { return &providers.UpdateDynuRequest{} }},
+	"Aliyun":       {Updater: providers.UpdateAliyun, Request: func() interface{} { return &providers.UpdateAliyunRequest{} }},
+	"AllInkl":      {Updater: providers.UpdateAllInkl, Request: func() interface{} { return &providers.UpdateAllInklRequest{} }},
+	"Cloudflare":   {Updater: providers.UpdateCloudflare, Request: func() interface{} { return &providers.UpdateCloudflareRequest{} }},
+	"DD24":         {Updater: providers.UpdateDD24, Request: func() interface{} { return &providers.UpdateDD24Request{} }},
+	"DigitalOcean": {Updater: providers.UpdateDigitalOcean, Request: func() interface{} { return &providers.UpdateDigitalOceanRequest{} }},
+	"DonDominio":   {Updater: providers.UpdateDonDominio, Request: func() interface{} { return &providers.UpdateDonDominioRequest{} }},
+	"DNSOMatic":    {Updater: providers.UpdateDNSOMatic, Request: func() interface{} { return &providers.UpdateDNSOMaticRequest{} }},
+	"DNSPod":       {Updater: providers.UpdateDNSPod, Request: func() interface{} { return &providers.UpdateDNSPodRequest{} }},
+	"Dreamhost":    {Updater: providers.UpdateDreamhost, Request: func() interface{} { return &providers.UpdateDreamhostRequest{} }},
+	"DuckDNS":      {Updater: providers.UpdateDuckDNS, Request: func() interface{} { return &providers.UpdateDuckDNSRequest{} }},
+	"DynDNS":       {Updater: providers.UpdateDynDNS, Request: func() interface{} { return &providers.UpdateDynDNSRequest{} }},
+	"FreeDNS":      {Updater: providers.UpdateFreeDNS, Request: func() interface{} { return &providers.UpdateFreeDNSRequest{} }},
+	"Gandi":        {Updater: providers.UpdateGandi, Request: func() interface{} { return &providers.UpdateGandiRequest{} }},
+	"GCP":          {Updater: providers.UpdateGCP, Request: func() interface{} { return &providers.UpdateGCPRequest{} }},
+	"GoDaddy":      {Updater: providers.UpdateGoDaddy, Request: func() interface{} { return &providers.UpdateGoDaddyRequest{} }},
+	"Google":       {Updater: providers.UpdateGoogle, Request: func() interface{} { return &providers.UpdateGoogleRequest{} }},
+	"He":           {Updater: providers.UpdateHe, Request: func() interface{} { return &providers.UpdateHeRequest{} }},
+	"Infomaniak":   {Updater: providers.UpdateInfomaniak, Request: func() interface{} { return &providers.UpdateInfomaniakRequest{} }},
+	"INWX":         {Updater: providers.UpdateINWX, Request: func() interface{} { return &providers.UpdateINWXRequest{} }},
+	"Linode":       {Updater: providers.UpdateLinode, Request: func() interface{} { return &providers.UpdateLinodeRequest{} }},
+	"LuaDNS":       {Updater: providers.UpdateLuaDNS, Request: func() interface{} { return &providers.UpdateLuaDNSRequest{} }},
+	"Namecheap":    {Updater: providers.UpdateNamecheap, Request: func() interface{} { return &providers.UpdateNamecheapRequest{} }},
+	"NoIP":         {Updater: providers.UpdateNoIP, Request: func() interface{} { return &providers.UpdateNoIPRequest{} }},
+	"Njalla":       {Updater: providers.UpdateNjalla, Request: func() interface{} { return &providers.UpdateNjallaRequest{} }},
+	"OpenDNS":      {Updater: providers.UpdateOpenDNS, Request: func() interface{} { return &providers.UpdateOpenDNSRequest{} }},
+	"OVH":          {Updater: providers.UpdateOVH, Request: func() interface{} { return &providers.UpdateOVHRequest{} }},
+	"Porkbun":      {Updater: providers.UpdatePorkbun, Request: func() interface{} { return &providers.UpdatePorkbunRequest{} }},
+	"Selfhost":     {Updater: providers.UpdateSelfhost, Request: func() interface{} { return &providers.UpdateSelfhostRequest{} }},
+	"Servercow":    {Updater: providers.UpdateServercow, Request: func() interface{} { return &providers.UpdateServercowRequest{} }},
+	"Spdyn":        {Updater: providers.UpdateSpdyn, Request: func() interface{} { return &providers.UpdateSpdynRequest{} }},
+	"Variomedia":   {Updater: providers.UpdateVariomedia, Request: func() interface{} { return &providers.UpdateVariomediaRequest{} }},
+}
+
+func IsSupported(p string) bool {
+	_, ok := updaters[p]
 	return ok
 }
 
-func sendHTTPRequest(method string, url string, auth *url.Userinfo) (string, error) {
-	httpClient := &http.Client{
-		Timeout: time.Second * 10,
+func GetUpdaters() []string {
+	var p []string
+	for k := range updaters {
+		p = append(p, k)
 	}
-	req, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		return "", fmt.Errorf("failed to create HTTP request: %v", err)
-	}
-	if auth != nil {
-		password, _ := auth.Password()
-		req.SetBasicAuth(auth.Username(), password)
-	}
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		return "", fmt.Errorf("failed to send HTTP request: %v", err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("failed to update DDNS entry: %s", resp.Status)
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", fmt.Errorf("failed to read HTTP response body: %v", err)
-	}
-	return string(bytes.TrimSpace(body)), nil
-}
-
-func updateStrato(job model.SyncJob, ipAddr string) error {
-	urlStr := fmt.Sprintf("https://%s:%s@dyndns.strato.com/nic/update?hostname=%s&myip=%s", job.User, job.Password, job.Domain, ipAddr)
-	resp, err := sendHTTPRequest(http.MethodGet, urlStr, nil)
-	if err != nil {
-		return err
-	}
-	switch {
-	case strings.Contains(resp, "good"):
-		return nil
-	case strings.Contains(resp, "nochg"):
-		return nil
-	default:
-		return fmt.Errorf("failed to update DDNS entry: %s", resp)
-	}
-}
-
-func updateDDNSS(job model.SyncJob, ipAddr string) error {
-	urlStr := fmt.Sprintf("https://www.ddnss.de/upd.php?user=%s&pwd=%s&host=%s&ip=%s&ip6=%s", job.User, job.Password, job.Domain, ipAddr, "")
-	resp, err := sendHTTPRequest(http.MethodGet, urlStr, nil)
-	if err != nil {
-		return err
-	}
-	if strings.Contains(resp, "Error Occurred While Processing Request") {
-		return fmt.Errorf("failed to update DDNS entry: %s", resp)
-	}
-	return nil
-}
-
-func updateDynu(job model.SyncJob, ipAddr string) error {
-	urlStr := fmt.Sprintf("https://%s:%s@api.dynu.com/nic/update?myip=%s&myipv6=", job.User, job.Password, ipAddr)
-	resp, err := sendHTTPRequest(http.MethodGet, urlStr, nil)
-	if err != nil {
-		return err
-	}
-	switch {
-	case strings.HasPrefix(resp, "good"):
-		return nil
-	case strings.HasPrefix(resp, "nochg"):
-		return nil
-	default:
-		return fmt.Errorf("failed to update DDNS entry: %s", resp)
-	}
-}
-
-func updateAliyun(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateAllInkl(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateCloudflare(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateDD24(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateDigitalOcean(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateDonDominio(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateDNSOMatic(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateDNSPod(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateDreamhost(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateDuckDNS(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateDynDNS(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateFreeDNS(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateGandi(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateGCP(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateGoDaddy(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateGoogle(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateHe(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateInfomaniak(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateINWX(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateLinode(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateLuaDNS(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateNamecheap(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateNoIP(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateNjalla(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateOpenDNS(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateOVH(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updatePorkbun(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateSelfhost(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateServercow(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateSpdyn(job model.SyncJob, ipAddr string) error {
-	return nil
-}
-
-func updateVariomedia(job model.SyncJob, ipAddr string) error {
-	return nil
+	return p
 }
