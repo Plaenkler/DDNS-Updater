853bcf99babeb1ff64eac49c92431ba7e782569a
Simon Lukas <60503970+Plaenkler@users.noreply.github.com>
2023-03-01 14:37:39 +0100

    [FIX] Remove provider fields

--- pkg/router/routes/api/job.go
+++ pkg/router/routes/api/job.go
@@ -1,14 +1,14 @@
 package api
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/plaenkler/ddns/pkg/database"
+	"github.com/plaenkler/ddns/pkg/database/model"
 	"github.com/plaenkler/ddns/pkg/ddns"
-	"github.com/plaenkler/ddns/pkg/model"
-	"golang.org/x/net/idna"
 	"gorm.io/gorm"
 )
 
@@ -17,43 +17,37 @@ func CreateJob(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	if err := r.ParseForm(); err != nil {
+	err := r.ParseForm()
+	if err != nil {
 		log.Printf("[api-CreateJob-1] could not parse form - error: %s", err)
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 	provider := r.FormValue("provider")
-	if !ddns.IsUpdaterSupported(provider) {
+	if !ddns.IsSupported(provider) {
 		log.Printf("[api-CreateJob-2] provider is not valid")
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
-	domain := r.FormValue("domain")
-	if _, err := idna.Lookup.ToASCII(domain); err != nil {
-		log.Printf("[api-CreateJob-3] domain is not valid - error: %s", err)
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-	user := r.FormValue("user")
-	password := r.FormValue("password")
-	if len(user) == 0 || len(password) == 0 {
-		log.Printf("[api-CreateJob-4] user or password is not set")
+	params := r.FormValue("params")
+	jobModel := ddns.GetUpdaters()[provider].Request
+	err = json.Unmarshal([]byte(params), &jobModel)
+	if err != nil {
+		log.Printf("[api-CreateJob-3] could not unmarshal params - error: %s", err)
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 	job := model.SyncJob{
 		Provider: provider,
-		Domain:   domain,
-		User:     user,
-		Password: password,
+		Params:   params,
 	}
-	if err := database.GetManager().DB.Create(&job).Error; err != nil {
-		log.Printf("[api-CreateJob-5] could not create job - error: %s", err)
+	err = database.GetManager().DB.Create(&job).Error
+	if err != nil {
+		log.Printf("[api-CreateJob-4] could not create job - error: %s", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
-	http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
-	log.Printf("[api-CreateJob-6] created job with ID %d", job.ID)
+	log.Printf("[api-CreateJob-5] created job with ID %d", job.ID)
 }
 
 func UpdateJob(w http.ResponseWriter, r *http.Request) {
@@ -67,26 +61,25 @@ func UpdateJob(w http.ResponseWriter, r *http.Request) {
 		log.Printf("[api-UpdateJob-1] could not parse form - error: %s", err.Error())
 		return
 	}
-	if !ddns.IsUpdaterSupported(r.FormValue("provider")) {
+	strID := r.FormValue("ID")
+	id, err := strconv.ParseUint(strID, 10, 32)
+	if err != nil {
+		http.Error(w, "ID is not valid", http.StatusBadRequest)
+		log.Printf("[api-UpdateJob-2] ID is not valid - error: %s", err.Error())
+		return
+	}
+	provider := r.FormValue("provider")
+	if !ddns.IsSupported(provider) {
 		http.Error(w, "Invalid provider", http.StatusBadRequest)
-		log.Printf("[api-UpdateJob-2] provider is not valid")
+		log.Printf("[api-UpdateJob-3] provider is not valid")
 		return
 	}
-	_, err = idna.Lookup.ToASCII(r.FormValue("domain"))
+	jobModel := ddns.GetUpdaters()[provider].Request
+	params := r.FormValue("params")
+	err = json.Unmarshal([]byte(params), &jobModel)
 	if err != nil {
-		http.Error(w, "Invalid domain", http.StatusBadRequest)
-		log.Printf("[api-UpdateJob-3] domain is not valid - error: %s", err)
-		return
-	}
-	if len(r.FormValue("user")) == 0 || len(r.FormValue("password")) == 0 {
-		http.Error(w, "User or password not set", http.StatusBadRequest)
-		log.Printf("[api-UpdateJob-4] user or password is not set")
-		return
-	}
-	id, err := strconv.ParseUint(r.FormValue("id"), 10, 32)
-	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		log.Printf("[api-UpdateJob-5] ID is not valid - error: %s", err)
+		http.Error(w, "Could not unmarshal params", http.StatusBadRequest)
+		log.Printf("[api-UpdateJob-4] could not unmarshal params - error: %s", err.Error())
 		return
 	}
 	job := model.SyncJob{
@@ -94,18 +87,16 @@ func UpdateJob(w http.ResponseWriter, r *http.Request) {
 			ID: uint(id),
 		},
 		Provider: r.FormValue("provider"),
-		Domain:   r.FormValue("domain"),
-		User:     r.FormValue("user"),
-		Password: r.FormValue("password"),
+		Params:   r.FormValue("params"),
 	}
 	err = database.GetManager().DB.Save(&job).Error
 	if err != nil {
 		http.Error(w, "Could not update job", http.StatusInternalServerError)
-		log.Printf("[api-UpdateJob-6] could not update job - error: %s", err)
+		log.Printf("[api-UpdateJob-5] could not update job - error: %s", err)
 		return
 	}
 	http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
-	log.Printf("[api-UpdateJob-7] updated job with ID %d", job.ID)
+	log.Printf("[api-UpdateJob-6] updated job with ID %d", job.ID)
 }
 
 func DeleteJob(w http.ResponseWriter, r *http.Request) {
@@ -117,7 +108,7 @@ func DeleteJob(w http.ResponseWriter, r *http.Request) {
 	}
 	id, err := strconv.ParseUint(strID, 10, 32)
 	if err != nil {
-		log.Printf("[api-DeleteJob-3] ID is not valid - error: %s", err)
+		log.Printf("[api-DeleteJob-2] ID is not valid - error: %s", err)
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
@@ -127,10 +118,10 @@ func DeleteJob(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 	if err := database.GetManager().DB.Unscoped().Delete(&job).Error; err != nil {
-		log.Printf("[api-DeleteJob-4] could not delete job - error: %s", err)
+		log.Printf("[api-DeleteJob-3] could not delete job - error: %s", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 	http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
-	log.Printf("[api-DeleteJob-5] deleted job with ID %d", job.ID)
+	log.Printf("[api-DeleteJob-4] deleted job with ID %d", job.ID)
 }
