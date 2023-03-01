8857bf269106caffdcf8b8780ce806257bb6b2dc
Simon Lukas <60503970+Plaenkler@users.noreply.github.com>
2023-03-01 14:35:11 +0100

    [UPD] Handle dynamic forms
    Co-Authored-By: Valentin Kaiser <60584607+Valentin-Kaiser@users.noreply.github.com>

--- pkg/router/routes/web/static/js/index.js
+++ pkg/router/routes/web/static/js/index.js
@@ -1,20 +1,35 @@
 // Load input`s values for edit job modal
-document.querySelectorAll('table tbody tr').forEach((row) => {
-  row.addEventListener('click', (event) => {
-    const [id, provider, domain, user] = event.target.parentNode.children
-    document.querySelector('#edit-id').value = id.textContent
-    document.querySelector('#edit-provider').value = provider.textContent
-    document.querySelector('#edit-domain').value = domain.textContent
-    document.querySelector('#edit-user').value = user.textContent
-    document.querySelector(
-      '#delete-button'
-    ).href = `/api/job/delete?ID=${id.textContent}`
-    new bootstrap.Modal(document.querySelector('#edit-modal')).show()
+function loadEdit(event) {
+  const [id, provider, domain, user] = event.target.parentNode.children
+  document.querySelector('#edit-id').value = id.textContent
+  document.querySelector('#edit-provider').value = provider.textContent
+  document.querySelector('#edit-domain').value = domain.textContent
+  document.querySelector('#edit-user').value = user.textContent
+  document.querySelector(
+    '#delete-button'
+  ).href = `/api/job/delete?ID=${id.textContent}`
+  new bootstrap.Modal(document.querySelector('#edit-modal')).show()
+}
+
+// Handle submit for different forms
+function handleForm(event) { 
+  event.preventDefault();
+  const provider = event.target.querySelector("#provider").value;
+  const inputs = event.target.querySelectorAll("input");
+  const data = {};
+  inputs.forEach(input => {
+    data[input.name] = input.value;
+  });
+  fetch(`/api/job/create?provider=${provider}&params=${JSON.stringify(data)}`, {
+    method: "POST",
   })
-})
+  .catch(error => {
+    console.error(error);
+  });
+}
 
 // Add inputs to form create job
-document.addEventListener("DOMContentLoaded", function() {
+function onLoad() {
   const providerDropdown = document.getElementById("provider");
   const contentContainer = document.getElementById("inputs-add-container");
   providerDropdown.addEventListener("change", function() {
@@ -24,13 +39,15 @@ document.addEventListener("DOMContentLoaded", function() {
       return
     }
     fetch(`/api/inputs?provider=${selectedProvider}`)
-    .then(response => response.text())
-    .then(data => {
-      contentContainer.innerHTML = data;
-      console.log(data);
-    })
     .catch(error => {
       console.error(error);
     });
   });
-});
+}
+
+document.getElementById("add-form").addEventListener("submit", handleForm);
+document.getElementById("edit-form").addEventListener("submit", handleForm);
+document.addEventListener("DOMContentLoaded", onLoad);
+document.querySelectorAll('table tbody tr').forEach((row) => {
+  row.addEventListener('click', loadTable)
+})
