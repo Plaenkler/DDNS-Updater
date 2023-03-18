8cfc69307c57e14e2c8a9685745f9d3c32dd9dca
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-03-18 19:12:36 +0100

    [UPD] Support Aliyun

--- go.mod
+++ go.mod
@@ -9,7 +9,14 @@ require (
 )
 
 require (
+	github.com/aliyun/alibaba-cloud-sdk-go v1.62.246 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-sqlite3 v1.14.15 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/opentracing/opentracing-go v1.2.1-0.20220228012449-10b1cf09e00b // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 )
--- pkg/ddns/providers/aliyun.go
+++ pkg/ddns/providers/aliyun.go
@@ -2,12 +2,55 @@ package providers
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/alidns"
 )
 
 type UpdateAliyunRequest struct {
-	// TODO implement
+	Domain       string `json:"Domain"`
+	Host         string `json:"Host"`
+	AccessKeyID  string `json:"AccessKeyID"`
+	AccessSecret string `json:"AccessSecret"`
+	Region       string `json:"Region"`
 }
 
 func UpdateAliyun(request interface{}, ipAddr string) error {
-	return fmt.Errorf("not implemented")
+	r, ok := request.(UpdateAliyunRequest)
+	if !ok {
+		return fmt.Errorf("invalid request type: %T", request)
+	}
+	client, err := alidns.NewClientWithAccessKey(r.Region, r.AccessKeyID, r.AccessSecret)
+	if err != nil {
+		return err
+	}
+	listRequest := alidns.CreateDescribeDomainRecordsRequest()
+	listRequest.Scheme = "https"
+	listRequest.DomainName = r.Domain
+	listRequest.RRKeyWord = r.Host
+	resp, err := client.DescribeDomainRecords(listRequest)
+	if err != nil {
+		return err
+	}
+	recordID := ""
+	for _, record := range resp.DomainRecords.Record {
+		if strings.EqualFold(record.RR, r.Host) {
+			recordID = record.RecordId
+			break
+		}
+	}
+	if recordID == "" {
+		return fmt.Errorf("no record found for host %s", r.Host)
+	}
+	updateRequest := alidns.CreateUpdateDomainRecordRequest()
+	updateRequest.Scheme = "https"
+	updateRequest.Value = ipAddr
+	updateRequest.Type = "A"
+	updateRequest.RR = r.Host
+	updateRequest.RecordId = recordID
+	_, err = client.UpdateDomainRecord(updateRequest)
+	if err != nil {
+		return err
+	}
+	return nil
 }
