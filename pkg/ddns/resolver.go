8a0ef2b8b24aead437f57e471c9abdc3b760e036
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-06-19 19:02:41 +0200

    [ADD] Resolver "my-ip"

--- pkg/ddns/resolver.go
+++ pkg/ddns/resolver.go
@@ -1,19 +1,31 @@
 package ddns
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
+var resolvers = map[string]string{
+	"ipify": "https://api.ipify.org",
+	"my-ip": "https://api.my-ip.io/ip",
+}
+
 func GetPublicIP() (string, error) {
-	resp, err := http.Get("https://api.ipify.org")
-	if err != nil {
-		return "", err
+	for r := range resolvers {
+		resp, err := http.Get(resolvers[r])
+		if err != nil {
+			log.Printf("[resolver-1] %s failed: %s", r, err)
+			continue
+		}
+		defer resp.Body.Close()
+		ipBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("[resolver-2] %s failed: %s", r, err)
+			continue
+		}
+		return string(ipBytes), nil
 	}
-	defer resp.Body.Close()
-	ipBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(ipBytes), nil
+	return "", fmt.Errorf("[resolver-3] all resolvers failed")
 }
