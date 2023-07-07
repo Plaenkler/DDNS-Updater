651a4841008b43c0df81c40a92f7b9a9bde7ea64
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-07-08 00:22:33 +0200

    [UPD] Service reconstruction

--- pkg/server/service.go
+++ pkg/server/service.go
@@ -15,49 +15,62 @@ import (
 )
 
 var (
-	static      embed.FS
-	managerOnce sync.Once
-	startOnce   sync.Once
-	instance    *Manager
+	lock   sync.Mutex
+	static embed.FS
+	router *http.ServeMux
 )
 
-type Manager struct {
-	Router *http.ServeMux
+func StartService() {
+	lock.Lock()
+	defer lock.Unlock()
+	err := initializeRouter()
+	if err != nil {
+		log.Fatalf("[server-StartService-1] could not initialize router: %v", err)
+	}
+	err = initializeServer()
+	if err != nil {
+		log.Fatalf("[server-StartService-2] could not initialize server: %v", err)
+	}
 }
 
-func GetManager() *Manager {
-	managerOnce.Do(func() {
-		instance = &Manager{
-			Router: http.NewServeMux(),
-		}
-	})
-	return instance
-}
-
-func (m *Manager) Start() {
-	startOnce.Do(func() {
-		m.initializeRouter()
-		err := m.provideFiles()
-		if err != nil {
-			log.Fatalf("[server-Start-1] could not provide files - error: %s", err)
-		}
-		m.initializeServer()
-	})
-}
-
-func (m *Manager) initializeRouter() {
+func initializeRouter() error {
 	r := NewRouter()
+	registerMiddlewares(r)
+	registerAPIRoutes(r)
+	router = r.ServeMux
+	err := registerStaticFiles(router)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func registerMiddlewares(r *Router) {
+	r.Use(forwardToProxy)
 	r.Use(limitRequests)
+}
+
+func registerAPIRoutes(r *Router) {
 	r.HandleFunc("/", web.ProvideIndex)
 	r.HandleFunc("/api/inputs", api.GetInputs)
 	r.HandleFunc("/api/job/create", api.CreateJob)
 	r.HandleFunc("/api/job/update", api.UpdateJob)
 	r.HandleFunc("/api/job/delete", api.DeleteJob)
 	r.HandleFunc("/api/config/update", api.UpdateConfig)
-	m.Router = r.ServeMux
 }
 
-func (m *Manager) initializeServer() {
+func registerStaticFiles(router *http.ServeMux) error {
+	fs, err := fs.Sub(static, "routes/web/static")
+	if err != nil {
+		return err
+	}
+	router.Handle("/js/", http.FileServer(http.FS(fs)))
+	router.Handle("/css/", http.FileServer(http.FS(fs)))
+	router.Handle("/img/", http.FileServer(http.FS(fs)))
+	return nil
+}
+
+func initializeServer() error {
 	config := config.GetConfig()
 	server := &http.Server{
 		Addr:              fmt.Sprintf(":%v", config.Port),
@@ -65,42 +78,11 @@ func (m *Manager) initializeServer() {
 		ReadHeaderTimeout: 5 * time.Second,
 		WriteTimeout:      15 * time.Second,
 		IdleTimeout:       120 * time.Second,
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Read X-Forwarded-For-Header for proxy requests
-			remoteIP := r.Header.Get("X-Forwarded-For")
-			remotePort := r.Header.Get("X-Forwarded-Port")
-			if remotePort == "" {
-				remotePort = "80"
-			}
-			r.URL.Scheme = "http"
-			r.URL.Host = fmt.Sprintf("%s:%s", remoteIP, remotePort)
-			m.Router.ServeHTTP(w, r)
-		}),
+		Handler:           router,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
-		log.Fatalf("[server-initializeServer-1] failed starting router - error: %s", err.Error())
-	}
-}
-
-func (m *Manager) provideFiles() error {
-	fs, err := fs.Sub(static, "routes/web/static")
-	if err != nil {
 		return err
 	}
-	m.Router.Handle("/js/", http.FileServer(http.FS(fs)))
-	m.Router.Handle("/css/", http.FileServer(http.FS(fs)))
-	m.Router.Handle("/img/", http.FileServer(http.FS(fs)))
 	return nil
-}
-
-func limitRequests(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := IsOverLimit(r)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusTooManyRequests)
-			return
-		}
-		handler.ServeHTTP(w, r)
-	})
 }
