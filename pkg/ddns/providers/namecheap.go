277f285a1f799a619c316b38272a55079709e1ed
tkyat <tommas.k@web.de>
2023-07-10 16:16:56 +0200

    i bims
    i bims

--- pkg/ddns/providers/namecheap.go
+++ pkg/ddns/providers/namecheap.go
@@ -2,13 +2,14 @@ package providers
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
 )
 
 type UpdateNamecheapRequest struct {
-	Domain        string
-	Host          string
-	Password      string
-	UseProviderIP bool
+	Domain   string
+	Host     string
+	Password string
 }
 
 func UpdateNamecheap(request interface{}, ipAddr string) error {
@@ -16,5 +17,14 @@ func UpdateNamecheap(request interface{}, ipAddr string) error {
 	if !ok {
 		return fmt.Errorf("invalid request type: %T", request)
 	}
-	return fmt.Errorf("not implemented %s", r.Domain)
+
+	urlStr := fmt.Sprintf("https://dynamicdns.park-your-domain.com/update?host=%s&domain=%s&password=%s&ip=%s", r.Host, r.Domain, r.Password, ipAddr)
+	resp, err := SendHTTPRequest(http.MethodGet, urlStr, nil)
+	if err != nil {
+		return err
+	}
+	if strings.Contains(resp, "success") {
+		return nil
+	}
+	return fmt.Errorf("failed to update DDNS entry: %s", resp)
 }
--- pkg/ddns/providers/noip.go
+++ pkg/ddns/providers/noip.go
@@ -1,57 +1,18 @@
-//package providers
-//
-//import (
-//	"fmt"
-//)
-//
-//type UpdateNoIPRequest struct {
-//    Domain        string
-//	  Host          string
-//	  Username      string
-//	  Password      string
-//	  UseProviderIP bool
-//}
-//
-//func UpdateNoIP(request interface{}, ipAddr string) error {
-//	  r, ok := request.(*UpdateNoIPRequest)
-//	  if !ok {
-//	  	return fmt.Errorf("invalid request type: %T", request)
-//	  }
-//	  return fmt.Errorf("not implemented %s", r.Domain)
-//}
-
-//Tom dem Seine Notizen von der Knowledgebase von NOIP.COM:
-
-//1: ExampleUpdateRequestString: "http://username:password@dynupdate.no-ip.com/nic/update?hostname=mytest.example.com&myip=192.0.2.25"
-
-//2: Raw HTTP GET Request:
-//"GET
-///nic/update?hostname=mytestyourhost.example.com&myip=192.0.2.25
-//HTTP/1.1
-//Host: dynupdate.no-ip.com
-//Authorization: Basic base-64-authorization
-//User-Agent: ##SEE SETTING the User-Agent Below##""
-
-//3: For software clients use the format:
-//"User-Agent: Company NameOfProgram/OSVersion-ReleaseVersion maintainer-contact@example.com"
-//User Agent is important! It helps us identify what Update Client is accessing us. This allows us to provide better technical support to YOUR USERS.
-
 package providers
 
 import (
 	"fmt"
 	"net/http"
 	"strings"
-
-	"github.com/plaenkler/ddns/pkg/util"
+	"io/ioutil"
 )
 
 type UpdateNoIPRequest struct {
-	// Domain        string
-	Host     string
-	Username string
-	Password string
-	// UseProviderIP bool
+	
+	Host          string
+	Username      string
+	Password      string
+	
 }
 
 func UpdateNoIP(request interface{}, ipAddr string) error {
@@ -59,15 +20,19 @@ func UpdateNoIP(request interface{}, ipAddr string) error {
 	if !ok {
 		return fmt.Errorf("invalid request type: %T", request)
 	}
-
-	//UpdateRequestString:
+	return fmt.Errorf("not implemented %s", r.Host)
+}
+	UsAg := "User-Agent: Plaenkler DDNS-Updater/V0 info@plaenkler.com" 
 	urlStr := fmt.Sprintf("http://%s:%s@dynupdate.no-ip.com/nic/update?hostname=%s&myip=%s", r.Username, r.Password, r.Host, ipAddr)
-	resp, err := util.SendHTTPRequest(http.MethodGet, urlStr, nil)
+	resp, err := SendHTTPRequest(http.MethodGet, urlStr, UsAg)
 	if err != nil {
 		return err
 	}
-	if strings.Contains(resp, "Error Occurred While Processing Request") {
-		return fmt.Errorf("failed to update DDNS entry: %s", resp)
-	}
-	return nil
-}
+	
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	
+	ErrMsg := fmt.Sprintf("When not succsessful, please resolve before trying again: %s/%s", err, body)
+
+	fmt.Println(ErrMsg)
+	
