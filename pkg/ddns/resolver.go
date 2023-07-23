8bf04194bbf0fb53459abc28920889c9b6b74211
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-07-23 14:36:52 +0200

    [UPD] Check if custom resolver available

--- pkg/ddns/resolver.go
+++ pkg/ddns/resolver.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/http"
 
+	"github.com/plaenkler/ddns-updater/pkg/config"
 	log "github.com/plaenkler/ddns-updater/pkg/logging"
 )
 
@@ -20,27 +21,43 @@ var resolvers = map[string]string{
 }
 
 func GetPublicIP() (string, error) {
+	cRes := config.GetConfig().Resolver
+	fmt.Printf("cRes: %s\n", cRes)
+	if cRes != "" {
+		addr, err := resolveIPAddress(cRes)
+		if err != nil {
+			return "", fmt.Errorf("[ddns-GetPublicIP-1] resolver %s failed: %s", cRes, err)
+		}
+		log.Infof("[ddns-GetPublicIP-2] %s succeeded: %s", cRes, addr)
+		return addr, nil
+	}
 	for r := range resolvers {
-		resp, err := http.Get(resolvers[r])
+		addr, err := resolveIPAddress(resolvers[r])
 		if err != nil {
-			log.Errorf("[ddns-GetPublicIP-1] %s failed: %s", r, err)
-			continue
-		}
-		defer resp.Body.Close()
-		bytes, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Errorf("[ddns-GetPublicIP-2] %s failed: %s", r, err)
-			continue
-		}
-		addr := string(bytes)
-		if !isValidIPAddress(addr) {
-			log.Errorf("[ddns-GetPublicIP-3] %s failed: %s", r, addr)
+			log.Errorf("[ddns-GetPublicIP-3] resolver %s failed: %s", r, err)
 			continue
 		}
 		log.Infof("[ddns-GetPublicIP-4] %s succeeded: %s", r, addr)
 		return addr, nil
 	}
 	return "", fmt.Errorf("[ddns-GetPublicIP-5] all resolvers failed")
+}
+
+func resolveIPAddress(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	addr := string(bytes)
+	if !isValidIPAddress(addr) {
+		return "", err
+	}
+	return addr, nil
 }
 
 func isValidIPAddress(ip string) bool {
