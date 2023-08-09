30e09da3c9db725e916e09bea956f536e957df98
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-08-09 22:08:15 +0200

    [ADD] Session service

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/plaenkler/ddns-updater/pkg/ddns"
 	log "github.com/plaenkler/ddns-updater/pkg/logging"
 	"github.com/plaenkler/ddns-updater/pkg/server"
+	"github.com/plaenkler/ddns-updater/pkg/server/session"
 )
 
 func main() {
@@ -15,15 +16,19 @@ func main() {
 	log.Infof("[main-main-1] started database connection")
 	go ddns.StartService()
 	log.Infof("[main-main-2] started ddns service")
+	go session.StartService()
+	log.Infof("[main-main-3] started session service")
 	go server.StartService()
-	log.Infof("[main-main-3] started webserver")
+	log.Infof("[main-main-4] started webserver")
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
 	server.StopService()
-	log.Infof("[main-main-4] stopped webserver")
+	log.Infof("[main-main-5] stopped webserver")
+	session.StopService()
+	log.Infof("[main-main-6] stopped session service")
 	ddns.StopService()
-	log.Infof("[main-main-5] stopped ddns service")
+	log.Infof("[main-main-7] stopped ddns service")
 	database.StopService()
-	log.Infof("[main-main-6] stopped database connection")
+	log.Infof("[main-main-8] stopped database connection")
 }
