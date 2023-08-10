9a9400cc08cd3f9c7deabcc3ce02818d8a3e6d4e
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-08-10 14:25:51 +0200

    [ADD] Middleware authenticate()

--- pkg/server/middles.go
+++ pkg/server/middles.go
@@ -3,6 +3,9 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/plaenkler/ddns-updater/pkg/server/session"
 )
 
 func limitRequests(handler http.Handler) http.Handler {
@@ -36,6 +39,34 @@ func forwardToProxy(handler http.Handler) http.Handler {
 func controlCache(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "public, max-age=86400")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		allowedPrefixes := []string{
+			"/css/",
+			"/img/",
+			"/login",
+			"/api/login",
+		}
+		pth := r.URL.Path
+		for _, path := range allowedPrefixes {
+			if strings.HasPrefix(pth, path) {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+		c, err := r.Cookie("ddns-updater-token")
+		if err != nil {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+		if !session.VerifySession(c.Value) {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
