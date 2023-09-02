2e6fb7e4e3fc5dd3e41916fd1b52a795c67e2c70
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-09-03 01:40:21 +0200

    [UPD] Use AES-GCM

--- pkg/cipher/cipher.go
+++ pkg/cipher/cipher.go
@@ -4,45 +4,58 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"errors"
+	"encoding/base64"
+	"fmt"
 	"io"
 )
 
-func Encrypt(key, plaintext []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
+func Encrypt(key string, plaintext string) (string, error) {
+	encrypter, err := aes.NewCipher([]byte(key))
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("[cipher-Encrypt-1] encryption failed: %s", err)
 	}
-	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
+	gcm, err := cipher.NewGCM(encrypter)
+	if err != nil {
+		return "", fmt.Errorf("[cipher-Encrypt-2] encryption failed: %s", err)
 	}
-	mode := cipher.NewCBCEncrypter(block, iv)
-	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
-	return ciphertext, nil
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return "", fmt.Errorf("[cipher-Encrypt-3] encryption failed: %s", err)
+	}
+	return base64.StdEncoding.EncodeToString(gcm.Seal(nonce, nonce, []byte(plaintext), nil)), nil
 }
 
-func Decrypt(key, ciphertext []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
+func Decrypt(key string, ciphertext string) (string, error) {
+	c, err := aes.NewCipher([]byte(key))
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("[cipher-Decrypt-1] decryption failed: %s", err)
 	}
-	if len(ciphertext) < aes.BlockSize {
-		return nil, errors.New("ciphertext too short")
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return "", fmt.Errorf("[cipher-Decrypt-2] decryption failed: %s", err)
 	}
-	iv := ciphertext[:aes.BlockSize]
-	ciphertext = ciphertext[aes.BlockSize:]
-	mode := cipher.NewCBCDecrypter(block, iv)
-	mode.CryptBlocks(ciphertext, ciphertext)
-	return ciphertext, nil
+	cipherBytes, err := base64.StdEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return "", fmt.Errorf("[cipher-Decrypt-3] decryption failed: %s", err)
+	}
+	nonceSize := gcm.NonceSize()
+	if len(cipherBytes) < nonceSize {
+		return "", fmt.Errorf("[cipher-Decrypt-4] decryption failed: %s", err)
+	}
+	nonce, cipherBytes := cipherBytes[:nonceSize], cipherBytes[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, cipherBytes, nil)
+	if err != nil {
+		return "", fmt.Errorf("[cipher-Decrypt-5] decryption failed: %s", err)
+	}
+	return string(plaintext), nil
 }
 
-func GenerateRandomKey() ([]byte, error) {
-	key := make([]byte, 32)
-	_, err := rand.Read(key)
+func GenerateRandomKey(length int) (string, error) {
+	randomBytes := make([]byte, length)
+	_, err := rand.Read(randomBytes)
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("[cipher-GenerateRandomKey-1] generating random key failed: %s", err)
 	}
-	return key, nil
+	return base64.URLEncoding.EncodeToString(randomBytes), nil
 }
