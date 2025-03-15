c8d5cbcdb6d5a312d93448eab807799d39095c8b
nandogva63 <168976619+nandogva63@users.noreply.github.com>
2025-03-15 22:40:19 +0100

    Update freedns.go
    Update provider freedns

--- pkg/ddns/providers/freedns.go
+++ pkg/ddns/providers/freedns.go
@@ -2,18 +2,27 @@ package providers
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
 )
 
 type UpdateFreeDNSRequest struct {
-	Domain string
-	Host   string
-	Token  string
+	Domain   string `json:"Domain"`
+	Token string `json:"Token"`
 }
 
-func UpdateFreeDNS(request interface{}, ipAddr string) error {
+func UpdateDD24(request interface{}, ipAddr string) error {
 	r, ok := request.(*UpdateFreeDNSRequest)
 	if !ok {
 		return fmt.Errorf("invalid request type: %T", request)
 	}
-	return fmt.Errorf("not implemented %s", r.Domain)
+	urlStr := fmt.Sprintf("https://freedns.afraid.org/dynamic/update.php?hostname=%s&Token", r.Host, r.Token)
+	resp, err := SendHTTPRequest(http.MethodGet, urlStr, nil)
+	if err != nil {
+		return err
+	}
+	if strings.Contains(resp, "success") {
+		return nil
+	}
+	return fmt.Errorf("failed to update DDNS entry: %s", resp)
 }
